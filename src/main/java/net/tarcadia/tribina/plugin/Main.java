84324d08084c9674e21f74fbefefbb9302e763cd
Tarcadia <tarcadia@qq.com>
2022-03-03 02:10:58 +0800

    the main

--- src/main/java/net/tarcadia/tribina/plugin/Main.java
+++ src/main/java/net/tarcadia/tribina/plugin/Main.java
@@ -1,8 +1,28 @@
 package net.tarcadia.tribina.plugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class Main extends JavaPlugin {
+
+    public static Main plugin = null;
+    public static FileConfiguration config = null;
+    public static PluginDescriptionFile descrp = null;
+    public static Logger logger = null;
+    public static String dataPath = null;
+
+    @Override
+    public void onLoad() {
+        Main.plugin = this;
+        Main.config = this.getConfig();
+        Main.descrp = this.getDescription();
+        Main.logger = this.getLogger();
+        Main.dataPath = this.getDataFolder().getPath();
+        Main.logger.info("Loaded " + Main.descrp.getName() + " v" + Main.descrp.getVersion() + ".");
+    }
 
     @Override
     public void onEnable() {
