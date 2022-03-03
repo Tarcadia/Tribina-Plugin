99061f62ec10baaafe1a90d7e52290be379d15fe
Tarcadia <tarcadia@qq.com>
2022-03-04 03:06:44 +0800

    clarified some implementation requirements

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/posset/BasePosSet.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/posset/BasePosSet.java
@@ -8,23 +8,26 @@ import java.util.List;
 
 public abstract class BasePosSet {
 
-    public abstract boolean contains(int x, int z);
-    public abstract boolean contains(@NotNull Pair<Integer, Integer> pos);
-    public abstract boolean containsAll(@NotNull Collection<? extends Pair<Integer, Integer>> poss);
+    protected long biasX;
+    protected long biasZ;
 
-    public abstract void add(int x, int z);
-    public abstract void add(@NotNull Pair<Integer, Integer> pos);
-    public abstract void addAll(@NotNull Collection<? extends Pair<Integer, Integer>> pSet);
+    public abstract boolean contains(long x, long z);
+    public abstract boolean contains(@NotNull Pair<Long, Long> pos);
+    public abstract boolean containsAll(@NotNull Collection<? extends Pair<Long, Long>> pSet);
+
+    public abstract void add(long x, long z);
+    public abstract void add(@NotNull Pair<Long, Long> pos);
+    public abstract void addAll(@NotNull Collection<? extends Pair<Long, Long>> pSet);
     public abstract void addAll(@NotNull BasePosSet pSet);
 
-    public abstract void sub(int x, int z);
-    public abstract void sub(@NotNull Pair<Integer, Integer> pos);
-    public abstract void subAll(@NotNull Collection<? extends Pair<Integer, Integer>> pSet);
+    public abstract void sub(long x, long z);
+    public abstract void sub(@NotNull Pair<Long, Long> pos);
+    public abstract void subAll(@NotNull Collection<? extends Pair<Long, Long>> pSet);
     public abstract void subAll(@NotNull BasePosSet pSet);
 
-    public abstract void cross(@NotNull Collection<? extends Pair<Integer, Integer>> pSet);
+    public abstract void cross(@NotNull Collection<? extends Pair<Long, Long>> pSet);
     public abstract void cross(@NotNull BasePosSet pSet);
 
-    public abstract List<Pair<Integer, Integer>> getList();
+    public abstract List<Pair<Long, Long>> getList();
 
 }
