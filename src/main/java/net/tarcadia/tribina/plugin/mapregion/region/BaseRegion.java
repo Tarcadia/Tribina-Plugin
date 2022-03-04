7718d30e2e602945b03e6a9fc6c2c55236946810
Tarcadia <tarcadia@qq.com>
2022-03-05 04:41:21 +0800

    deal with file and load here in a region class

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/region/BaseRegion.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/region/BaseRegion.java
@@ -2,24 +2,208 @@ package net.tarcadia.tribina.plugin.mapregion.region;
 
 import net.tarcadia.tribina.plugin.mapregion.posset.GlobalPosSet;
 import net.tarcadia.tribina.plugin.util.data.configuration.Configuration;
+import net.tarcadia.tribina.plugin.util.func.Bitmaps;
+import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Set;
 
 public class BaseRegion {
 
-    private final String filePath;
-    private final Configuration config;
-    private final GlobalPosSet map;
+    static String KEY_LOC_LOC = "loc.loc";
+    static String KEY_LOC_OFFSET_X = "loc.offset.x";
+    static String KEY_LOC_OFFSET_Z = "loc.offset.z";
 
-    public BaseRegion(@NotNull String name, @NotNull String fileRoot, int ttl) {
-        this.filePath = fileRoot + "/" + name;
-        this.config = new Configuration(this.filePath + ".yml", ttl);
-        this.map = new GlobalPosSet(this.filePath + ".bmp");
+    protected final File fileConfig;
+    protected final File fileBitmap;
+    protected final Configuration config;
+    protected final GlobalPosSet map;
+
+    protected final Location loc;
+    protected final long biasX;
+    protected final long biasZ;
+
+    protected final boolean isNull;
+
+    public BaseRegion() {
+        this.fileConfig = null;
+        this.fileBitmap = null;
+        this.config = null;
+        this.map = null;
+        this.loc = null;
+        this.biasX = 0;
+        this.biasZ = 0;
+        this.isNull = true;
     }
 
-    public BaseRegion(@NotNull String name, @NotNull String fileRoot) {
-        this.filePath = fileRoot + "/" + name;
-        this.config = new Configuration(this.filePath + ".yml");
-        this.map = new GlobalPosSet(this.filePath + ".bmp");
+    public BaseRegion(@NotNull String name, @NotNull Path fileRoot, int ttl) {
+        this.fileConfig = new File(fileRoot + "/" + name + ".yml");
+        this.fileBitmap = new File(fileRoot + "/" + name + ".bmp");
+        this.config = new Configuration(fileConfig, ttl);
+        this.map = new GlobalPosSet();
+        if (this.config.isLocation(KEY_LOC_LOC) &&
+                this.config.isLong(KEY_LOC_OFFSET_X) &&
+                this.config.isLong(KEY_LOC_OFFSET_Z)
+        ) {
+            Location loc = null;
+            long biasX = 0;
+            long biasZ = 0;
+            boolean isNull = true;
+            try {
+                long offsetX = this.config.getLong(KEY_LOC_OFFSET_X);
+                long offsetZ = this.config.getLong(KEY_LOC_OFFSET_Z);
+                loc = this.config.getLocation(KEY_LOC_LOC);
+                biasX = loc.getBlockX() + offsetX;
+                biasZ = loc.getBlockZ() + offsetZ;
+                var posSet = Bitmaps.loadBmpToSet(fileBitmap);
+                for (var pos : posSet) {
+                    this.map.add(pos.x() + biasX, pos.y() + biasZ);
+                }
+                isNull = false;
+            } catch (Exception e) {
+                loc = null;
+                biasX = 0;
+                biasZ = 0;
+                isNull = true;
+            }
+            this.loc = loc;
+            this.biasX = biasX;
+            this.biasZ = biasZ;
+            this.isNull = isNull;
+        } else {
+            this.loc = null;
+            this.biasX = 0;
+            this.biasZ = 0;
+            this.isNull = true;
+        }
+    }
+
+    public BaseRegion(@NotNull String name, @NotNull Path fileRoot) {
+        this.fileConfig = new File(fileRoot + "/" + name + ".yml");
+        this.fileBitmap = new File(fileRoot + "/" + name + ".bmp");
+        this.config = new Configuration(fileConfig);
+        this.map = new GlobalPosSet();
+        if (this.config.isLocation(KEY_LOC_LOC) &&
+                this.config.isLong(KEY_LOC_OFFSET_X) &&
+                this.config.isLong(KEY_LOC_OFFSET_Z)
+        ) {
+            Location loc = null;
+            long biasX = 0;
+            long biasZ = 0;
+            boolean isNull = true;
+            try {
+                long offsetX = this.config.getLong(KEY_LOC_OFFSET_X);
+                long offsetZ = this.config.getLong(KEY_LOC_OFFSET_Z);
+                loc = this.config.getLocation(KEY_LOC_LOC);
+                biasX = loc.getBlockX() + offsetX;
+                biasZ = loc.getBlockZ() + offsetZ;
+                var posSet = Bitmaps.loadBmpToSet(fileBitmap);
+                for (var pos : posSet) {
+                    this.map.add(pos.x() + biasX, pos.y() + biasZ);
+                }
+                isNull = false;
+            } catch (Exception e) {
+                loc = null;
+                biasX = 0;
+                biasZ = 0;
+                isNull = true;
+            }
+            this.loc = loc;
+            this.biasX = biasX;
+            this.biasZ = biasZ;
+            this.isNull = isNull;
+        } else {
+            this.loc = null;
+            this.biasX = 0;
+            this.biasZ = 0;
+            this.isNull = true;
+        }
+    }
+
+    public BaseRegion(@NotNull File fileConfig, @NotNull File fileBitmap, int ttl) {
+        this.fileConfig = fileConfig;
+        this.fileBitmap = fileBitmap;
+        this.config = new Configuration(fileConfig, ttl);
+        this.map = new GlobalPosSet();
+        if (this.config.isLocation(KEY_LOC_LOC) &&
+                this.config.isLong(KEY_LOC_OFFSET_X) &&
+                this.config.isLong(KEY_LOC_OFFSET_Z)
+        ) {
+            Location loc = null;
+            long biasX = 0;
+            long biasZ = 0;
+            boolean isNull = true;
+            try {
+                long offsetX = this.config.getLong(KEY_LOC_OFFSET_X);
+                long offsetZ = this.config.getLong(KEY_LOC_OFFSET_Z);
+                loc = this.config.getLocation(KEY_LOC_LOC);
+                biasX = loc.getBlockX() + offsetX;
+                biasZ = loc.getBlockZ() + offsetZ;
+                var posSet = Bitmaps.loadBmpToSet(fileBitmap);
+                for (var pos : posSet) {
+                    this.map.add(pos.x() + biasX, pos.y() + biasZ);
+                }
+                isNull = false;
+            } catch (Exception e) {
+                loc = null;
+                biasX = 0;
+                biasZ = 0;
+                isNull = true;
+            }
+            this.loc = loc;
+            this.biasX = biasX;
+            this.biasZ = biasZ;
+            this.isNull = isNull;
+        } else {
+            this.loc = null;
+            this.biasX = 0;
+            this.biasZ = 0;
+            this.isNull = true;
+        }
+    }
+
+    public BaseRegion(@NotNull File fileConfig, @NotNull File fileBitmap) {
+        this.fileConfig = fileConfig;
+        this.fileBitmap = fileBitmap;
+        this.config = new Configuration(fileConfig);
+        this.map = new GlobalPosSet();
+        if (this.config.isLocation(KEY_LOC_LOC) &&
+                this.config.isLong(KEY_LOC_OFFSET_X) &&
+                this.config.isLong(KEY_LOC_OFFSET_Z)
+        ) {
+            Location loc = null;
+            long biasX = 0;
+            long biasZ = 0;
+            boolean isNull = true;
+            try {
+                long offsetX = this.config.getLong(KEY_LOC_OFFSET_X);
+                long offsetZ = this.config.getLong(KEY_LOC_OFFSET_Z);
+                loc = this.config.getLocation(KEY_LOC_LOC);
+                biasX = loc.getBlockX() + offsetX;
+                biasZ = loc.getBlockZ() + offsetZ;
+                var posSet = Bitmaps.loadBmpToSet(fileBitmap);
+                for (var pos : posSet) {
+                    this.map.add(pos.x() + biasX, pos.y() + biasZ);
+                }
+                isNull = false;
+            } catch (Exception e) {
+                loc = null;
+                biasX = 0;
+                biasZ = 0;
+                isNull = true;
+            }
+            this.loc = loc;
+            this.biasX = biasX;
+            this.biasZ = biasZ;
+            this.isNull = isNull;
+        } else {
+            this.loc = null;
+            this.biasX = 0;
+            this.biasZ = 0;
+            this.isNull = true;
+        }
     }
 
     // TODO: Finish the implementation
