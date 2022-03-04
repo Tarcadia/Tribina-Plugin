587f04e4dfe7527a11b1bdcaa12d1848b067799f
Tarcadia <tarcadia@qq.com>
2022-03-05 03:20:04 +0800

    Test, passed

---
+++ src/test/java/net/tarcadia/tribina/plugin/playauth/AuthTest.java
@@ -0,0 +1,44 @@
+package net.tarcadia.tribina.plugin.playauth;
+
+import net.tarcadia.tribina.plugin.util.data.configuration.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthTest {
+
+    private void sleep(long millis) {
+        var t = System.currentTimeMillis();
+        while (System.currentTimeMillis() < t + millis) {
+            try {
+                Thread.sleep(t + millis - System.currentTimeMillis());
+            } catch (InterruptedException e) {}
+        }
+    }
+
+    @Test
+    void doTest() {
+        Auth auth = new Auth("src/test/resources/playauth/AuthTest/A.yml", 500);
+        Configuration config = new Configuration("src/test/resources/playauth/AuthTest/A.yml");
+
+        System.out.println(auth.getAuth("play"));
+        System.out.println(auth.getAuth("play.place.dirt"));
+        System.out.println(auth.getAuth("play.place.grass"));
+        System.out.println(auth.getAuth("play.place.flower"));
+        System.out.println(auth.getAuth("play.break.dirt"));
+        System.out.println(auth.getAuth("play.break.grass"));
+        System.out.println(auth.getAuth("play.break.flower"));
+        config.set("play.break.flower", "play_break_flower");
+        System.out.println(auth.getAuth("play.break.flower"));
+        sleep(500);
+        System.out.println(auth.getAuth("play.break.flower"));
+        config.set("play.break.flower", null);
+        System.out.println(auth.getAuth("play.place"));
+        System.out.println(auth.getAuth("play.break"));
+        System.out.println(auth.getAuth("play.place*"));
+        sleep(500);
+        System.out.println(auth.getAuth("play.place"));
+        System.out.println(auth.getAuth("play.break"));
+        System.out.println(auth.getAuth("play.place*"));
+    }
+}
