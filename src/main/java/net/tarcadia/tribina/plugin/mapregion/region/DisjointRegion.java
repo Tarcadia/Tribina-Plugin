8db03a2a774e1d4ab17c0ae8aae515c55e071e7b
Tarcadia <tarcadia@qq.com>
2022-03-05 21:37:27 +0800

    better view

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/region/DisjointRegion.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/region/DisjointRegion.java
@@ -10,7 +10,11 @@ public class DisjointRegion extends BaseRegion {
     protected final String id;
     protected final List<? extends BaseRegion> peers;
 
-    public DisjointRegion(@NotNull String regionId, @NotNull Path fileRoot, @NotNull List<? extends BaseRegion> peers) {
+    public DisjointRegion(
+            @NotNull String regionId,
+            @NotNull Path fileRoot,
+            @NotNull List<? extends BaseRegion> peers
+    ) {
         super(regionId, fileRoot);
         this.id = regionId;
         this.peers = peers;
