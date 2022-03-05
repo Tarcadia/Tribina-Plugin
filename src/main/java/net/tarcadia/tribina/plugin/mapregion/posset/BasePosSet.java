b69228e46c192a3d35cd79318889295d936061d2
Tarcadia <tarcadia@qq.com>
2022-03-05 20:27:59 +0800

    a new method implementation required

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/posset/BasePosSet.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/posset/BasePosSet.java
@@ -25,6 +25,8 @@ public abstract class BasePosSet {
 
     public abstract void cross(@NotNull Collection<? extends Pair<Long, Long>> pSet);
     public abstract void cross(@NotNull BasePosSet pSet);
+    public abstract void decross(@NotNull Set<? extends Pair<Long, Long>> pSet);
+    public abstract void decross(@NotNull BasePosSet pSet);
 
     public abstract Long minX();
     public abstract Long minZ();
