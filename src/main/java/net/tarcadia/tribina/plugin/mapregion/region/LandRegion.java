2d3627d88b3e0947e68363ec56bea65f596ce5de
Tarcadia <tarcadia@qq.com>
2022-03-05 22:41:36 +0800

    Use BaseDisjointRegion

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/region/LandRegion.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/region/LandRegion.java
@@ -1,103 +1,18 @@
 package net.tarcadia.tribina.plugin.mapregion.region;
 
-import net.tarcadia.tribina.plugin.mapregion.posset.BasePosSet;
-import net.tarcadia.tribina.plugin.util.type.Pair;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 
-public class LandRegion extends BaseRegion implements DisjointRegion {
-
-    protected final String id;
-    protected final List<? extends BaseRegion> peers;
+public class LandRegion extends BaseDisjointRegion {
 
     public LandRegion(
             @NotNull String regionId,
             @NotNull Path fileRoot,
-            @NotNull List<? extends BaseRegion> peers
+            @NotNull List<? extends DisjointRegion> peers
     ) {
-        super(regionId, fileRoot);
-        this.id = regionId;
-        this.peers = peers;
-    }
-
-    @Override
-    public void add(long x, long z) {
-        if (!this.isNull) {
-            boolean flag = true;
-            for (var peer : this.peers) flag &= !peer.contains(x, z);
-            if (flag) {
-                this.map.add(x, z);
-                this.saveMap();
-            }
-        }
-    }
-
-    @Override
-    public void add(@NotNull Pair<Long, Long> pos) {
-        if (!this.isNull) {
-            boolean flag = true;
-            for (var peer : this.peers) flag &= !peer.contains(pos);
-            if (flag) {
-                this.map.add(pos);
-                this.saveMap();
-            }
-        }
-    }
-
-    @Override
-    public void addAll(@NotNull Collection<? extends Pair<Long, Long>> pSet) {
-        if (!this.isNull) {
-            var set = new HashSet<Pair<Long, Long>>(pSet);
-            for (var peer : this.peers) set.removeAll(peer.getSet());
-            this.map.addAll(set);
-            this.saveMap();
-        }
-    }
-
-    @Override
-    public void addAll(@NotNull BasePosSet pSet) {
-        if (!this.isNull) {
-            var set = new HashSet<>(pSet.getSet());
-            for (var peer : this.peers) set.removeAll(peer.getSet());
-            this.map.addAll(set);
-            this.saveMap();
-        }
-    }
-
-    public void addOver(long x, long z) {
-        if (!this.isNull) {
-            for (var peer : this.peers) peer.sub(x, z);
-            this.map.add(x, z);
-            this.saveMap();
-        }
-    }
-
-    public void addOver(@NotNull Pair<Long, Long> pos) {
-        if (!this.isNull) {
-            for (var peer : this.peers) peer.sub(pos);
-            this.map.add(pos);
-            this.saveMap();
-        }
-    }
-
-    public void addAllOver(@NotNull Collection<? extends Pair<Long, Long>> pSet) {
-        if (!this.isNull) {
-            for (var peer : this.peers) peer.subAll(pSet);
-            this.map.addAll(pSet);
-            this.saveMap();
-        }
-    }
-
-    public void addAllOver(@NotNull BasePosSet pSet) {
-        if (!this.isNull) {
-            for (var peer : this.peers) peer.subAll(pSet);
-            this.map.addAll(pSet);
-            this.saveMap();
-        }
+        super(regionId, fileRoot, peers);
     }
 
 }
