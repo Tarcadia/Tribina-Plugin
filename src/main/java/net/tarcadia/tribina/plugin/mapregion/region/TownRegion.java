baa3984dd82d8b58cf0e4ebc3adad06b655f8387
Tarcadia <tarcadia@qq.com>
2022-03-05 23:14:04 +0800

    please use reLoc and reBias to modify

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/region/TownRegion.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/region/TownRegion.java
@@ -1,4 +1,23 @@
 package net.tarcadia.tribina.plugin.mapregion.region;
 
-public class TownRegion extends DisjointRegion{
+import net.tarcadia.tribina.plugin.mapregion.region.base.BaseDisjointRegion;
+import net.tarcadia.tribina.plugin.mapregion.region.base.BaseRegion;
+import net.tarcadia.tribina.plugin.mapregion.region.base.DisjointRegion;
+import net.tarcadia.tribina.plugin.mapregion.region.base.DisjointSubRegion;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.util.List;
+
+public class TownRegion extends BaseDisjointRegion implements DisjointRegion {
+    public TownRegion(
+            @NotNull String regionId,
+            @NotNull Path fileRoot,
+            @NotNull List<? extends DisjointRegion> peers
+    ) {
+        super(regionId, fileRoot, peers);
+    }
+
+    // TODO: More implements that make town works.
+
 }
