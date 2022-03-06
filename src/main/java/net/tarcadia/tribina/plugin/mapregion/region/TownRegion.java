70d3731b9f802bbec6ff4f2c6cd9434aa172607c
Tarcadia <tarcadia@qq.com>
2022-03-07 01:35:14 +0800

    add and modify constructors

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/region/TownRegion.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/region/TownRegion.java
@@ -1,19 +1,35 @@
 package net.tarcadia.tribina.plugin.mapregion.region;
 
-import net.tarcadia.tribina.plugin.mapregion.region.base.BaseDisjointRegion;
+import net.tarcadia.tribina.plugin.mapregion.region.base.BaseDisjointParentRegion;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.util.List;
 
-public class TownRegion extends BaseDisjointRegion {
+public class TownRegion extends BaseDisjointParentRegion {
+
+    private final List<? extends AssetRegion> assets;
 
     public TownRegion(
             @NotNull String regionId,
             @NotNull Path fileRoot,
-            @NotNull List<? extends AssetRegion> peers
+            @NotNull List<? extends TownRegion> peers,
+            @NotNull List<? extends AssetRegion> assets
     ) {
-        super(regionId, fileRoot, peers);
+        super(regionId, fileRoot, peers, assets);
+        this.assets = assets;
+    }
+
+    public TownRegion(
+            @NotNull String regionId,
+            @NotNull File fileConfig,
+            @NotNull File fileBitmap,
+            @NotNull List<? extends TownRegion> peers,
+            @NotNull List<? extends AssetRegion> assets
+    ) {
+        super(regionId, fileConfig, fileBitmap, peers, assets);
+        this.assets = assets;
     }
 
     // TODO: More implements that make town works.
