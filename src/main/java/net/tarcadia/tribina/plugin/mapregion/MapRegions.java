e16a2bea12591520f8ce3cba5926e6bc06b51b49
Tarcadia <tarcadia@qq.com>
2022-03-07 01:55:19 +0800

    add some implements of loading process

--- src/main/java/net/tarcadia/tribina/plugin/mapregion/MapRegions.java
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/MapRegions.java
@@ -8,6 +8,7 @@ import net.tarcadia.tribina.plugin.util.data.configuration.Configuration;
 import net.tarcadia.tribina.plugin.wasted.mapregions.Main;
 
 import java.io.File;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MapRegions {
@@ -16,16 +17,73 @@ public class MapRegions {
     public static final String KEY_REGION_TOWN_LIST = "regions.town";
     public static final String KEY_REGION_LAND_LIST = "regions.land";
 
+    public static final String PATH_MAPREGIONS = "/MapRegions/";
+    public static final String PATH_FILE_CONFIG = PATH_MAPREGIONS + "/config.yml";
+    public static final String PATH_REGION_PATHS = PATH_MAPREGIONS + "/Paths/";
+    public static final String PATH_REGION_LANDS = PATH_MAPREGIONS + "/Lands/";
+    public static final String PATH_REGION_TOWNS = PATH_MAPREGIONS + "/Towns/";
+
     public static Configuration config = null;
     public static List<PathRegion> regionPaths = null;
     public static List<LandRegion> regionLands = null;
     public static List<TownRegion> regionTowns = null;
     public static List<AssetRegion> regionAssets = null;
 
-    public void load()
-    {
-        MapRegions.config = new Configuration(new File(Main.dataPath + "/MapRegions.yml"));
-        MapRegions.config.getStringList("paths");
+    public void load() {
+        this.loadConfig();
+        this.loadPaths();
+        this.loadLands();
+        this.loadTowns();
+    }
+
+    public void loadConfig() {
+        MapRegions.config = new Configuration(new File(Main.dataPath + PATH_FILE_CONFIG));
+    }
+
+    public void loadPaths() {
+        if (MapRegions.config != null) {
+            var configSec = MapRegions.config.getConfigurationSection(KEY_REGION_PATH_LIST);
+            if (configSec != null) for (var id : configSec.getKeys(false)) {
+                var fileConfig = new File(Main.dataPath + PATH_REGION_PATHS + id + ".yml");
+                var fileBitmap = new File(Main.dataPath + PATH_REGION_PATHS + id + ".bmp");
+                var pathRegion = new PathRegion(id, fileConfig, fileBitmap);
+                MapRegions.regionPaths.add(pathRegion);
+            }
+        }
+    }
+
+    public void loadLands() {
+        if (MapRegions.config != null) {
+            var configSec = MapRegions.config.getConfigurationSection(KEY_REGION_LAND_LIST);
+            if (configSec != null) for (var id : configSec.getKeys(false)) {
+                var fileConfig = new File(Main.dataPath + PATH_REGION_LANDS + id + ".yml");
+                var fileBitmap = new File(Main.dataPath + PATH_REGION_LANDS + id + ".bmp");
+                var landRegion = new LandRegion(id, fileConfig, fileBitmap, MapRegions.regionLands);
+                MapRegions.regionLands.add(landRegion);
+            }
+        }
+    }
+
+    public void loadTowns() {
+        if (MapRegions.config != null) {
+            var configSec = MapRegions.config.getConfigurationSection(KEY_REGION_TOWN_LIST);
+            if (configSec != null) for (var id : configSec.getKeys(false)) {
+                var subConfigSec = configSec.getConfigurationSection(id);
+                var fileConfig = new File(Main.dataPath + PATH_REGION_TOWNS + id + ".yml");
+                var fileBitmap = new File(Main.dataPath + PATH_REGION_TOWNS + id + ".bmp");
+                var assets = new LinkedList<AssetRegion>();
+                var townRegion = new TownRegion(id, fileConfig, fileBitmap, MapRegions.regionTowns, assets);
+                MapRegions.regionTowns.add(townRegion);
+
+                if (subConfigSec != null) for (var subId : subConfigSec.getKeys(false)) {
+                    var subFileConfig = new File(Main.dataPath + PATH_REGION_TOWNS + id + "/" + subId + ".yml");
+                    var subFileBitmap = new File(Main.dataPath + PATH_REGION_TOWNS + id + "/" + subId + ".bmp");
+                    var assetRegion = new AssetRegion(subId, subFileConfig, subFileBitmap, assets, townRegion);
+                    assets.add(assetRegion);
+                }
+                MapRegions.regionAssets.addAll(assets);
+            }
+        }
     }
 
 }
