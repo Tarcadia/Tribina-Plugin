a0cc4166ffdd1405073ac17457e01e601c96f3f9
Tarcadia <tarcadia@qq.com>
2022-03-07 03:05:09 +0800

    Event Listener for generally all key events, and call sub systems' api to do something.

---
+++ src/main/java/net/tarcadia/tribina/plugin/EventsListener.java
@@ -0,0 +1,19 @@
+package net.tarcadia.tribina.plugin;
+
+import net.tarcadia.tribina.plugin.util.BaseListener;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+public class EventsListener extends BaseListener {
+
+    public EventsListener() {
+        super();
+    }
+
+    public EventsListener(@NotNull JavaPlugin plugin) {
+        super(plugin);
+    }
+
+
+
+}
