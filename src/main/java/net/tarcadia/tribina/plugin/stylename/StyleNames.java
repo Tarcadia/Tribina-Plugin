5bcad5c5a020741fddf7c7996918eda716c1d5a9
Tarcadia <tarcadia@qq.com>
2022-03-08 13:46:22 +0800

    modified name and string consts

--- src/main/java/net/tarcadia/tribina/plugin/stylename/StyleNames.java
+++ src/main/java/net/tarcadia/tribina/plugin/stylename/StyleNames.java
@@ -9,16 +9,19 @@ import java.io.File;
 
 public class StyleNames {
 
-    public static final String KEY_STYLENAME_ENABLED = "enabled";
-    public static final String KEY_STYLENAME_NAME = ".name";
-    public static final String KEY_STYLENAME_TAG = ".tag";
-    public static final String KEY_STYLENAME_TAG_LIST = ".tag-list";
-    public static final String KEY_STYLENAME_TAG_VISIBLE = ".tag-visible";
-    public static final String KEY_STYLENAME_STYLE = ".style";
-    public static final String KEY_STYLENAME_STYLE_LIST = ".style-list";
+    public static final String SN = "[SN] ";
 
-    public static final String PATH_STYLENAMES = "/StyleNames/";
-    public static final String PATH_FILE_CONFIG = PATH_STYLENAMES + "/config.yml";
+    public static final String KEY_STYLENAMES_ENABLED = "enabled";
+    public static final String KEY_STYLENAMES_PLAYERS = "players.";
+    public static final String KEY_STYLENAMES_NAME = ".name";
+    public static final String KEY_STYLENAMES_TAG = ".tag";
+    public static final String KEY_STYLENAMES_TAG_LIST = ".tag-list";
+    public static final String KEY_STYLENAMES_TAG_VISIBLE = ".tag-visible";
+    public static final String KEY_STYLENAMES_STYLE = ".style";
+    public static final String KEY_STYLENAMES_STYLE_LIST = ".style-list";
+
+    public static final String PATH_ROOT = "/StyleNames/";
+    public static final String PATH_FILE_CONFIG = PATH_ROOT + "/config.yml";
 
     private static Configuration config;
 
@@ -32,13 +35,13 @@ public class StyleNames {
 
     public static String getDisplay(@NotNull Player player) {
 
-        var style = StyleNames.config.getString(player.getName() + KEY_STYLENAME_STYLE, "");
-        var name = StyleNames.config.getString(player.getName() + KEY_STYLENAME_NAME, player.getName());
-        var tag = StyleNames.config.getString(player.getName() + KEY_STYLENAME_TAG);
-        var tagVisible = StyleNames.config.getBoolean(player.getName() + KEY_STYLENAME_TAG_VISIBLE);
+        var style = StyleNames.config.getString(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE, "");
+        var name = StyleNames.config.getString(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_NAME, player.getName());
+        var tag = StyleNames.config.getString(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG);
+        var tagVisible = StyleNames.config.getBoolean(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG_VISIBLE);
 
-        var styleLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_STYLE_LIST);
-        var tagLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_TAG_LIST);
+        var styleLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE_LIST);
+        var tagLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG_LIST);
 
         Style theStyle;
         Tag theTag;
@@ -50,7 +53,7 @@ public class StyleNames {
                 theStyle = Style.Normal;
             }
         } catch (IllegalArgumentException e) {
-            Main.logger.warning("[SN] Style " + style + " load failed.");
+            Main.logger.warning(SN + "Style " + style + " load failed.");
             theStyle = Style.Normal;
         }
 
@@ -61,7 +64,7 @@ public class StyleNames {
                 theTag = Tag.NullTag;
             }
         } catch (IllegalArgumentException e) {
-            Main.logger.warning("[SN] Tag " + tag + " load failed.");
+            Main.logger.warning(SN + "Tag " + tag + " load failed.");
             theTag = Tag.NullTag;
         }
 
@@ -80,42 +83,42 @@ public class StyleNames {
     }
 
     public static void addStyle(@NotNull Player player, @NotNull String style) {
-        var styleLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_STYLE_LIST);
+        var styleLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE_LIST);
         styleLst.add(style);
-        StyleNames.config.set(player.getName() + KEY_STYLENAME_STYLE_LIST, styleLst);
+        StyleNames.config.set(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE_LIST, styleLst);
     }
 
     public static void addTag(@NotNull Player player, @NotNull String tag) {
-        var tagLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_TAG_LIST);
+        var tagLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG_LIST);
         tagLst.add(tag);
-        StyleNames.config.set(player.getName() + KEY_STYLENAME_STYLE_LIST, tagLst);
+        StyleNames.config.set(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE_LIST, tagLst);
     }
 
     public static void setName(@NotNull Player player, @NotNull String name) {
-        StyleNames.config.set(player.getName() + KEY_STYLENAME_NAME, name);
+        StyleNames.config.set(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_NAME, name);
     }
 
     public static void setStyle(@NotNull Player player, @NotNull String style) {
-        var styleLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_STYLE_LIST);
+        var styleLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE_LIST);
         if (styleLst.contains(style)) {
-            StyleNames.config.set(player.getName() + KEY_STYLENAME_STYLE, style);
+            StyleNames.config.set(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE, style);
         }
     }
 
     public static void setTag(@NotNull Player player, @NotNull String tag) {
-        var tagLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_TAG_LIST);
+        var tagLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG_LIST);
         if (tagLst.contains(tag)) {
-            StyleNames.config.set(player.getName() + KEY_STYLENAME_TAG, tag);
+            StyleNames.config.set(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG, tag);
         }
     }
 
     public static boolean checkStyle(@NotNull Player player, @NotNull String style) {
-        var styleLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_STYLE_LIST);
+        var styleLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_STYLE_LIST);
         return styleLst.contains(style);
     }
 
     public static boolean checkTag(@NotNull Player player, @NotNull String tag) {
-        var tagLst = StyleNames.config.getStringList(player.getName() + KEY_STYLENAME_TAG_LIST);
+        var tagLst = StyleNames.config.getStringList(KEY_STYLENAMES_PLAYERS + player.getName() + KEY_STYLENAMES_TAG_LIST);
         return tagLst.contains(tag);
     }
 
