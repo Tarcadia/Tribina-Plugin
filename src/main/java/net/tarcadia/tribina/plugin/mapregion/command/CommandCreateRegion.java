4c5850e4be1fe3cdccc3dfde1c98c26459212d33
Tarcadia <tarcadia@qq.com>
2022-03-10 23:48:11 +0800

    Implement these class to do login functions

---
+++ src/main/java/net/tarcadia/tribina/plugin/mapregion/command/CommandCreateRegion.java
@@ -0,0 +1,67 @@
+package net.tarcadia.tribina.plugin.mapregion.command;
+
+import net.tarcadia.tribina.plugin.mapregion.MapRegions;
+import net.tarcadia.tribina.plugin.util.BaseCommand;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class CommandCreateRegion extends BaseCommand implements TabExecutor {
+
+    public CommandCreateRegion(@NotNull String cmd) {
+        super(cmd);
+    }
+
+    public CommandCreateRegion(@NotNull JavaPlugin plugin, @NotNull String cmd) {
+        super(plugin, cmd);
+    }
+
+    /**
+     * Executes the given command, returning its success.
+     * <br>
+     * If false is returned, then the "usage" plugin.yml entry for this command
+     * (if defined) will be sent to the player.
+     *
+     * @param sender  Source of the command
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used
+     * @param args    Passed command arguments
+     * @return true if a valid command, otherwise false
+     */
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if ((sender instanceof Player) && (args.length == 2)) {
+            Location loc = ((Player) sender).getLocation().getBlock().getLocation();
+            MapRegions.createLandRegion(args[0], loc);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Requests a list of possible completions for a command argument.
+     *
+     * @param sender  Source of the command.  For players tab-completing a
+     *                command inside of a command block, this will be the player, not
+     *                the command block.
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used
+     * @param args    The arguments passed to the command, including final
+     *                partial argument to be completed
+     * @return A List of possible completions for the final argument, or null
+     * to default to the command executor
+     */
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return null;
+    }
+}
