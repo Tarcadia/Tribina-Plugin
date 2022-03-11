5e21846b2f4a39bfef71d06fc59da1c0e267a502
Tarcadia <tarcadia@qq.com>
2022-03-12 00:13:28 +0800

    added Functional.java that it will work as a command executor and an event listener

---
+++ Erod/LogIn/src/main/java/net/tarcadia/tribina/erod/login/Functional.java
@@ -0,0 +1,30 @@
+package net.tarcadia.tribina.erod.login;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class Functional implements CommandExecutor, Listener {
+    /**
+     * Executes the given command, returning its success.
+     * <br>
+     * If false is returned, then the "usage" plugin.yml entry for this command
+     * (if defined) will be sent to the player.
+     *
+     * @param sender  Source of the command
+     * @param command Command which was executed
+     * @param label   Alias of the command which was used
+     * @param args    Passed command arguments
+     * @return true if a valid command, otherwise false
+     */
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+
+}
