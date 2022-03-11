328e5af226e79e00983270854cfc8121d03748ce
Tarcadia <tarcadia@qq.com>
2022-03-11 23:41:48 +0800

    Flatten the structure and reform the functional subsystems. I found it possible to let each subsystem to determin the auth by themselves. The only problem is that useful classes like Configuration and Auth have to be different copies in each subsystem and are not able to sync the modification. However, these seem not a big problem, and the good of flatten the structure is way much more significant than these shortages.

---
+++ Erod/LogIn/src/main/java/net/tarcadia/tribina/erod/login/LogIn.java
@@ -0,0 +1,17 @@
+package net.tarcadia.tribina.erod.login;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LogIn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
