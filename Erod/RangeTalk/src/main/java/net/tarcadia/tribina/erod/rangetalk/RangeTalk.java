dbcdb9c70e91c3e6dfddeb372f9bab61b327ca76
Tarcadia <tarcadia@qq.com>
2022-03-12 05:09:01 +0800

    implemented RangeTalk

--- Erod/RangeTalk/src/main/java/net/tarcadia/tribina/erod/rangetalk/RangeTalk.java
+++ Erod/RangeTalk/src/main/java/net/tarcadia/tribina/erod/rangetalk/RangeTalk.java
@@ -1,17 +1,94 @@
 package net.tarcadia.tribina.erod.rangetalk;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
-public final class RangeTalk extends JavaPlugin {
+import java.io.File;
+import java.util.logging.Logger;
+
+public final class RangeTalk extends JavaPlugin implements Listener {
+
+    public static RangeTalk plugin = null;
+    public static Configuration config = null;
+    public static PluginDescriptionFile descrp = null;
+    public static Logger logger = null;
+    public static String dataPath = null;
+
+    public static final String KEY_ENABLED = "enabled";
+    public static final String KEY_DEFAULT_RANGE = "default-range";
+    public static final String KEY_PLAYERS = "players.";
+    public static final String KEY_PLAYERS_RANGE = ".range";
+
+    public boolean isFunctionEnabled() {
+        return config.getBoolean(KEY_ENABLED);
+    }
+
+    public void functionEnable() {
+        config.set(KEY_ENABLED, true);
+        logger.info("Plugin functional enabled.");
+    }
+
+    public void functionDisable() {
+        config.set(KEY_ENABLED, false);
+        logger.info("Plugin functional disabled.");
+    }
+
+    @Override
+    public void onLoad() {
+        this.saveDefaultConfig();
+        plugin = this;
+        config = Configuration.getConfiguration(new File(this.getConfig().getCurrentPath()));
+        descrp = this.getDescription();
+        logger = this.getLogger();
+        dataPath = this.getDataFolder().getPath() + "/";
+        this.saveDefaultConfig();
+        logger.info("Loaded " + descrp.getName() + " v" + descrp.getVersion() + ".");
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        this.getServer().getPluginManager().registerEvents(this, this);
+        logger.info("Enabled " + descrp.getName() + " v" + descrp.getVersion() + ".");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        logger.info("Disabled " + descrp.getName() + " v" + descrp.getVersion() + ".");
     }
+
+    private Double getRange(@NotNull Player player) {
+        if (!config.contains(KEY_PLAYERS + player.getName())) {
+            config.set(KEY_PLAYERS + player.getName() + KEY_PLAYERS_RANGE, config.getDouble(KEY_DEFAULT_RANGE));
+        }
+        return config.getDouble(KEY_PLAYERS + player.getName() + KEY_PLAYERS_RANGE);
+    }
+
+    private void setRange(@NotNull Player player, double range) {
+        config.set(KEY_PLAYERS + player.getName() + KEY_PLAYERS_RANGE, range);
+    }
+
+    public boolean checkRange(@NotNull Player player1, @NotNull Player player2) {
+        var r = this.getRange(player1);
+        var loc1 = player1.getLocation();
+        var loc2 = player2.getLocation();
+        var sqrX = (loc1.getX() - loc2.getX()) * (loc1.getX() - loc2.getX());
+        var sqrY = (loc1.getY() - loc2.getY()) * (loc1.getY() - loc2.getY());
+        var sqrZ = (loc1.getZ() - loc2.getZ()) * (loc1.getZ() - loc2.getZ());
+        var sqrR = r * r;
+        return (loc1.getWorld() == loc2.getWorld()) && (sqrX + sqrY + sqrZ < sqrR);
+    }
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        var player = event.getPlayer();
+        var recipients = event.getRecipients();
+        recipients.removeIf(p -> !this.checkRange(player, p));
+    }
+
 }
